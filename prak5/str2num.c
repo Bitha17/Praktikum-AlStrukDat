1eda1495946fa1fb90ee4a56c2b9be0be269d1cd
Bitha17 <16521076@mahasiswa.itb.ac.id>
2022-10-06 13:44:37 +0700

    prak

--- prak5/str2num.c
+++ prak5/str2num.c
@@ -9,17 +9,37 @@
 #include "boolean.h"
 #include <string.h>
 
+boolean same(Word w, char *s){
+	if (strlen(s) == w.Length){
+		for (int j = 0; j < w.Length; j++){
+			if (w.TabWord[j] != s[j]){
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
+}
+
 boolean isNum(Word w, char *angka[]){
-	for (int i = 0; i<15; i++){
-		if (strcmp(w.TabWord,angka[i]) == 0){
+	for (int i = 0; i<16; i++){
+		if (same(w, angka[i])){
 			return true;
 		}
 	}
 	return false;
 }
 
+int satuan(Word w, char *angka[]){
+	for (int i = 0; i<10; i++){
+		if (same(w,angka[i])){
+			return (i+1);
+		}
+	}
+}
+
 int main(){
-	char *angka[] = {"satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas", "belas", "puluh", "seratus", "ratus"};
+	char *angka[] = {"satu", "dua", "tiga", "empat", "lima", "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas", "belas", "puluh", "seratus", "ratus", "nol"};
 	int num;
 	endWord = false;
 	STARTWORD();
@@ -27,42 +47,58 @@ int main(){
 		if (isNum(currentWord, angka)){
 			num = 0;
 			while(isNum(currentWord, angka)){
-				if (strcmp(currentWord.TabWord,"seratus") == 0){
+				if (same(currentWord, "nol")){
+					ADVWORD();
+					if (endWord) break;
+				}
+				else if (same(currentWord, "seratus")){
 					num += 100;
 					ADVWORD();
+					if (endWord) break;
 				}
-				else if(strcmp(currentWord.TabWord,"sepuluh") == 0){
+				else if(same(currentWord, "sepuluh")){
 					num += 10; 
 					ADVWORD();
+					if (endWord) break;
 				}
-				else if(strcmp(currentWord.TabWord,"sebelas") == 0){
+				else if(same(currentWord,"sebelas")){
 					num += 11;
 					ADVWORD();
+					if (endWord) break;
 				}
 				else{
-					for (int i = 0; i<10; i++){
-						if (strcmp(currentWord.TabWord,angka[i])==0){
-							ADVWORD();
-							if (strcmp(currentWord.TabWord,"ratus")==0){
-								num = (i+1)*100;
-								ADVWORD();
-							}
-							else if (strcmp(currentWord.TabWord,"puluh")==0){
-								num = (i+1)*10;
-								ADVWORD();
-							}
-							else if (strcmp(currentWord.TabWord,"belas")==0){
-								num = i+11;
-								ADVWORD();
-							}
-							else{
-								num = i+1;
-							}
-						}
+					int temp = satuan(currentWord,angka);
+					ADVWORD();
+					if (endWord) {
+						num += temp;
+						break;
 					}
-				}	
+					else if (same(currentWord,"ratus")){
+						num += temp*100;
+						ADVWORD();
+						if (endWord) break;
+					}
+					else if (same(currentWord,"puluh")){
+						num += temp*10;
+					 	ADVWORD();
+						if (endWord) break;
+					}
+					else if (same(currentWord,"belas")){
+					 	num += temp + 10;
+					 	ADVWORD();
+						if (endWord) break;
+					}
+					else{
+					 	num += temp;
+					}
+				}
+			}	
+			if (!endWord){
+				printf("%d ", num);
 			}
-			printf("%d ", num);
+			else{
+				printf("%d", num);
+			}
 		}
 		else{
 			for (int i = 0; i < currentWord.Length; i++){
